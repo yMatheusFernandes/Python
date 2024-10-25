11118acaf81a142e77d979df0151347bf5f27126
matheus <matheuz1398@gmail.com>
2024-10-25 19:07:31 -0300

    Update desafio 3.py

--- exercicios/ex001/desafio 3.py
+++ exercicios/ex001/desafio 3.py
@@ -1,3 +1,4 @@
-n1 = input('Diga um numero')
-n2 = input('Diga outro numero')
-print('A soma entre os dois numeros e',n1 + n2)
+n1 = int(input('Diga um numero'))
+n2 = int(input('Diga outro numero'))
+n3 = n1+n2
+print(f"A soma entre os dois números, {n1} + {n2}, é {n3}")
